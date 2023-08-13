b05e9a4813b6ee6e8fde636684c76732882003d3
umezinwa vincent <umezvincent20@gmail.com>
2023-08-13 07:34:03 -0700

    first push, did basic codes

---
+++ teulogia.js
@@ -0,0 +1,9 @@
+// hamburger menu
+function appear() {
+	document.querySelector(".mobile_nav").style.left = "0";
+	console.log("appear");
+}
+function closex() {
+	document.querySelector(".mobile_nav").style.left = "100%";
+	console.log("close");
+}
